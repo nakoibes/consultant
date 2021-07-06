f23a4a4528fa80c8ce0d10cf5f35231a6d848d14
nakoibes <alefanow.d@yandex.ru>
2021-07-06 15:19:05 +0300

    api search

---
+++ test.py
@@ -0,0 +1,20 @@
+import json
+from pprint import pprint
+from pymongo import MongoClient
+import requests
+
+mongo_client = MongoClient(host="localhost", port=27017)
+db = mongo_client.get_database("consultant")
+key = "6b6d3dc1db81eb304b998af41ef6e91d47b3bb5f"
+q = "Сбербанк"
+inn = "7721581040"
+j = {"inn": "614570014372",
+     "requestDate": "2021-06-06"}
+# r = requests.get(f"https://api-fns.ru/api/search?q={q}&key={key}", )
+# r = requests.get(f"https://api-fns.ru/api/fl_status?inn=614570014372&key={key}")
+# r = requests.get(f"https://api-fns.ru/api/egr?req={inn}&key={key}",)
+r = requests.post("https://statusnpd.nalog.ru/api/v1/tracker/taxpayer_status", json=j)
+text = r.text
+
+db.get_collection("col").insert_one(json.loads(text))
+pprint(text)
